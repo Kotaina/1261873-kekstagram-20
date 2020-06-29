2bb21194d3c4dafdc16ca0b6e66de76e0a3d9407
Kotaina <kotaina217+git@gmail.com>
2020-06-29 20:09:13 +0300

    -comments

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 
 var OBJECT_COUNT = 25;
 
-var randomiser = function (start, end) {
-  var randomElement = Math.floor(Math.random() * end) + start;
-  return randomElement;
-};
+var documentPhotoObjects = [];
+
+var descriptions = ['Наконец-то дома!', 'Мой типичный день:', 'Воспоминания: 2 года назад', 'Надеюсь веруться'];
+
+var names = ['Виктор', 'Енисей', 'Святогор', 'Дубрав'];
 
 var messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -13,33 +14,53 @@ var messages = ['Всё отлично!', 'В целом всё неплохо. 
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var names = ['Виктор', 'Енисей', 'Святогор', 'Дубрав'];
-
-var userComment = {
-  avatar: 'img/avatar-' + randomiser(1, 6) + '.svg',
-  message: messages[randomiser(1, 6)],
-  name: names[randomiser(1, 4)]
+var randomiser = function (start, end) {
+  var randomElement = Math.floor(Math.random() * end) + start;
+  return randomElement;
 };
 
-var customObject = {
-  url: 'photos/' + randomiser(1, 25) + '.jpg',
-  description: 'описание',
-  likes: randomiser(15, 200),
-  comments: userComment
+var generateCommentObject = function () {
+  var userComment = {
+    avatar: 'img/avatar-' + randomiser(1, 6) + '.svg',
+    message: messages[randomiser(1, messages.length)],
+    name: names[randomiser(1, names.length)]
+  };
+  return userComment;
 };
 
-var createCustomObject = [];
+var generatePhotoObject = function () {
+  var photoObject = {
+    url: 'photos/' + randomiser(1, 25) + '.jpg',
+    description: descriptions,
+    likes: randomiser(15, 200),
+    comments: generateCommentObject()
+  };
+  return photoObject;
+};
 
-var pictureList = document.querySelector('.pictures');
-var userPictureTemplate = document.querySelector('#picture');
+var createPhotoData = function () {
+  for (var i = 0; i < OBJECT_COUNT; i++) {
+    documentPhotoObjects.push(generatePhotoObject());
+  }
+};
 
-
-for (var i = 0; i < OBJECT_COUNT; i++) {
+var fillPhotosTemplate = function (photoObject) {
+  var userPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var userPhoto = userPictureTemplate.cloneNode(true);
-  createCustomObject.push(customObject);
-  userPhoto.querySelector('src').value = customObject[i].url;
-  userPhoto.querySelector('.picture__likes').textContent = customObject[i].likes;
-  userPhoto.querySelector('.picture__comments').textContent = customObject[i].userComment;
+  userPhoto.querySelector('.picture__img').src = photoObject.url;
+  userPhoto.querySelector('.picture__likes').textContent = photoObject.likes;
+  userPhoto.querySelector('.picture__comments').textContent = photoObject.comments;
+  return userPhoto;
+};
 
-  pictureList.appendChild(userPhoto);
-}
+var renderPhotoGrid = function (userPhotoElement) {
+  var pictureList = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < OBJECT_COUNT; i++) {
+    fragment.appendChild(fillPhotosTemplate(userPhotoElement[i]));
+  }
+  pictureList.appendChild(fragment);
+};
+
+createPhotoData();
+renderPhotoGrid(documentPhotoObjects);
